1b54b7058e254488985c64a9a6766532fe222f0b
CuriousCI <ionutcicio.dev@gmail.com>
2022-02-08 11:44:26 +0100

    Update script to fetch events

--- public/script.js
+++ public/script.js
@@ -1,22 +1,45 @@
+// var app = {
+//     init: function () {
+//         // Use origin and pathname to force to use https and avoid mixed content
+//         let origin = window.location.origin;
+//         let pathname = window.location.pathname;
+//         $.getJSON(`${origin}${pathname}?users.json`)
+//             .done(app.writeUsers)
+//             .fail(app.onFail);
+//     },
+//     onFail: function (error) {
+//         console.log("errore nella lettura del file json");
+//         console.log(error);
+//     },
+//     writeUsers: function (jsonData) {
+//         console.log(jsonData);
+//         for (user of jsonData) {
+//             $("ul").append(`<li>${user.user_id}: <span class="cool">${user.name}</span></li>`);
+//         }
+//     }
+// }
+
 var app = {
-    init: function () {
-        // Use origin and pathname to force to use https and avoid mixed content
-        let origin = window.location.origin;
-        let pathname = window.location.pathname;
-        $.getJSON(`${origin}${pathname}?users.json`)
-            .done(app.writeUsers)
-            .fail(app.onFail);
+    init: _ => {
+        let origin = window.location.origin,
+            pathname = window.location.origin
+
+        $.getJSON(`${origin}${pathname}?events`)
+            .done(app.writeEvents)
+            .fail(app.onFail)
     },
-    onFail: function (error) {
-        console.log("errore nella lettura del file json");
-        console.log(error);
+
+    onFail: error => {
+        console.log("errore nella lettura del file json")
+        console.log(error)
     },
-    writeUsers: function (jsonData) {
-        console.log(jsonData);
-        for (user of jsonData) {
-            $("ul").append(`<li>${user.user_id}: <span class="cool">${user.name}</span></li>`);
-        }
+
+    writeEvents: jsonData => {
+        console.log(jsonData)
+        for (event_ of jsonData)
+            $("main").append(`<div>${event_.name}</div>`)
     }
+
 }
 
 
